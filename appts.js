d19734d772bbb79a6526c05dc3b6bc03c3f7ae6c
chosun <chosun@oregonstate.edu>
2019-08-13 22:21:56 +0900

    revert back to id

--- appts.js
+++ appts.js
@@ -14,10 +14,10 @@ module.exports = function(){
         });
     }
 
-    ///mysql.pool.query("SELECT bsg_people.character_id as id, fname, lname, bsg_planets.name AS homeworld, age FROM bsg_people INNER JOIN bsg_planets ON homeworld = bsg_planets.planet_id", function(error, results, fields)
-
+    //the following query works in phpAdmin to show 
+    //mysql.pool.query("SELECT dc_appt.appt_id as id, appt_date, patient_id, dc_doctor.name AS doctor, assist_id, appt_reason, appt_result, next_appt_date, bill_id FROM dc_appt INNER JOIN dc_doctor ON doctor = dc_doctor.doctor_id", function(error, results, fields){
     function getAppts(res, mysql, context, complete){
-        mysql.pool.query("SELECT dc_appt.appt_id as id, appt_date, patient_id, dc_doctor.name AS doctor, assist_id, appt_reason, appt_result, next_appt_date, bill_id FROM dc_appt INNER JOIN dc_doctor ON doctor = dc_doctor.doctor_id", function(error, results, fields){
+        mysql.pool.query("SELECT dc_appt.appt_id as id, appt_date, patient_id, doctor, assist_id, appt_reason, appt_result, next_appt_date, bill_id FROM dc_appt", function(error, results, fields){
             if(error){
                 res.write(JSON.stringify(error));
                 res.end();
