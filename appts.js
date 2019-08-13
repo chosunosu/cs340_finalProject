07cbfb7955e4b041006c9a73682905a293a7a512
chosun <chosun@oregonstate.edu>
2019-08-13 21:41:11 +0900

    update on filter function

--- appts.js
+++ appts.js
@@ -100,7 +100,7 @@ module.exports = function(){
 
     /*lists appointment by doctor*/
     
-    function getApptbyDoctor(req, res, mysql, context, complete){
+    function getApptsbyDoctor(req, res, mysql, context, complete){
         var query = "SELECT dc_appt.appt_id as id, patient_id, dc_doctor.name AS doctor_id FROM dc_appt INNER JOIN dc_doctor ON doctor_id = dc_doctor.doctor_id WHERE dc_appt.doctor = ?";
         console.log(req.params)
         var inserts = [req.params.doctor]
@@ -113,6 +113,23 @@ module.exports = function(){
               complete();
           });
       }
+
+    /*Display all appointments from a given doctor. Requires web based javascript to delete users with AJAX*/
+    router.get('/filter/:doctor', function(req, res){
+        var callbackCount = 0;
+        var context = {};
+        context.jsscripts = ["deleteperson.js","filterpeople.js","searchpeople.js"];
+        var mysql = req.app.get('mysql');
+        getApptsbyDoctor(req,res, mysql, context, complete);
+        getDoctors(res, mysql, context, complete);
+        function complete(){
+            callbackCount++;
+            if(callbackCount >= 2){
+                res.render('appts', context);
+            }
+
+        }
+    });
 
         /* Adds an appointment, redirects to the appointments page after adding */
 
