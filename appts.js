2520ac607657135496136907a2295cfd3faacefb
chosun <chosun@oregonstate.edu>
2019-08-13 19:21:45 +0900

    Update appts.js

--- appts.js
+++ appts.js
@@ -2,13 +2,42 @@ module.exports = function(){
     var express = require('express');
     var router = express.Router();
 
-    /*Display all appointments from a given homeworld. Requires web based javascript to delete users with AJAX*/
-    router.get('/filter/:homeworld', function(req, res){
+    function getDoctors(res, mysql, context, complete){
+        mysql.pool.query("SELECT doctor_id as id FROM dc_doctor", function(error, results, fields){
+            if(error){
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.appts  = results;
+            complete();
+        });
+    }
+
+
+    ///mysql.pool.query("SELECT bsg_people.character_id as id, fname, lname, bsg_planets.name AS homeworld, age FROM bsg_people INNER JOIN bsg_planets ON homeworld = bsg_planets.planet_id", function(error, results, fields){
+
+    function getAppts(res, mysql, context, complete){
+        mysql.pool.query("SELECT dc_appt.appt_id as id, appt_date, patient_id, doctor_id, assist_id, appt_reason, appt_result, next_appt_date, bill_id FROM dc_appt", function(error, results, fields){
+            if(error){
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.appts = results;
+            complete();
+        });
+    }
+
+
+    
+
+    /*Display all appointments. Requires web based javascript to delete users with AJAX*/
+
+    router.get('/', function(req, res){
         var callbackCount = 0;
         var context = {};
         context.jsscripts = ["deleteappt.js","filterappts.js","searchappts.js"];
         var mysql = req.app.get('mysql');
-        getApptsbyDoctor(req,res, mysql, context, complete);
+        getAppts(res, mysql, context, complete);
         getDoctors(res, mysql, context, complete);
         function complete(){
             callbackCount++;
