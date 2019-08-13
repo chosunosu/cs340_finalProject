844eb100f6590e9e5a5895a75d476ec5da0edf35
hmb815 <52272860+hmb815@users.noreply.github.com>
2019-08-13 17:42:57 +0900

    Update planets.js

--- planets.js
+++ planets.js
@@ -2,52 +2,52 @@ module.exports = function(){
     var express = require('express');
     var router = express.Router();
 
-    function getPlanets(res, mysql, context, complete){
-      mysql.pool.query("SELECT planet_id as id, name FROM bsg_planets", function(error, results, fields){
+    function getDoctors(res, mysql, context, complete){
+      mysql.pool.query("SELECT doctor_id as id, name FROM dc_doctor", function(error, results, fields){
           if(error){
               res.write(JSON.stringify(error));
               res.end();
           }
-          context.planets  = results;
+          context.doctors  = results;
           complete();
       });
     }
 
-    
-    function servePlanets(req, res){
-        console.log("You asked me for some planets?")
-        var query = 'SELECT planet_id, name, population, language, capital FROM bsg_planets';
+
+    function serveDoctors(req, res){
+        console.log("Searching for doctors?")
+        var query = 'SELECT doctor_id, name, specialty, ext FROM dc_doctor';
         var mysql = req.app.get('mysql');
         var context = {};
 
-        function handleRenderingOfPlanets(error, results, fields){
+        function handleRenderingOfDoctors(error, results, fields){
           console.log(error)
           console.log(results)
           console.log(fields)
           //take the results of that query and store ti inside context
-          context.planets = results;
+          context.doctors = results;
           //pass it to handlebars to put inside a file
-          res.render('planets', context)
+          res.render('doctors', context)
         }
         //execute the sql query
-        mysql.pool.query(query, handleRenderingOfPlanets)
+        mysql.pool.query(query, handleRenderingOfDoctors)
 
         //res.send('Here you go!');
     }
 
-    function serveOnePlanet(chicken, steak) {
+    function serveOneDoctor(chicken, steak) {
       console.log(chicken.params.fancyId);
       console.log(chicken.params);
       fancyId = chicken.params.fancyId
 
-      var queryString = "SELECT planet_id, name, population, language, capital FROM bsg_planets WHERE planet_id = ?"
+      var queryString = "SELECT doctor_id, name, specialty, ext FROM dc_doctor WHERE doctor_id = ?"
 
       var mysql = steak.app.get('mysql')
       var context = {};
 
-      function handleRenderingOfOnePlanet(error, results, fields){
+      function handleRenderingOfOneDoctor(error, results, fields){
           console.log("results are " + results)
-          context.planet = results[0]
+          context.doctor = results[0]
           console.log(context)
 
           if(error){
@@ -55,11 +55,11 @@ module.exports = function(){
             steak.write(error)
             steak.end();
           }else{
-            steak.render('serverPlanet',context);
+            steak.render('serverDoctor',context);
           }
       }
       //execute the query
-      var queryString = mysql.pool.query(queryString, fancyId, handleRenderingOfOnePlanet);
+      var queryString = mysql.pool.query(queryString, fancyId, handleRenderingOfOneDoctor);
 
       //steak.send("Here's a good tasty well done steak");
     }
@@ -68,21 +68,21 @@ module.exports = function(){
       console.log(req.body.homeworld)
       console.log(req.body)
       var mysql = req.app.get('mysql');
-      var sql = "INSERT INTO bsg_planets (name, population, language, capital) VALUES (?,?,?,?)";
-      var inserts = [req.body.name, req.body.population, req.body.language, req.body.capital];
+      var sql = "INSERT INTO dc_doctor (name, specialty, ext) VALUES (?,?,?)";
+      var inserts = [req.body.name, req.body.specialty, req.body.ext];
       sql = mysql.pool.query(sql,inserts,function(error, results, fields){
           if(error){
               console.log(JSON.stringify(error))
               res.write(JSON.stringify(error));
               res.end();
           }else{
-              res.redirect('/planets');
+              res.redirect('/doctors');
           }
       });
   });
 
-    router.get('/', servePlanets);
-    
+    router.get('/', serveDoctors);
+
     /*
     router.get('/', function(req, res){
       var callbackCount = 0;
@@ -91,7 +91,7 @@ module.exports = function(){
       var mysql = req.app.get('mysql');
       //getPeople(res, mysql, context, complete);
       getPlanets(res, mysql, context, complete);
-      
+
       function complete(){
           callbackCount++;
           if(callbackCount >= 2){
@@ -99,10 +99,10 @@ module.exports = function(){
           }
 
       }
-      
+
     });
     */
-    
+
     //router.get('/:fancyId', serveOnePlanet);
     return router;
 }();
