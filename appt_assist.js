8168b3c55ba757b083bf9cceaed422153fac9127
hmb815 <52272860+hmb815@users.noreply.github.com>
2019-08-16 02:22:39 +0900

    Update appt_assist.js

--- appt_assist.js
+++ appt_assist.js
@@ -2,19 +2,19 @@ module.exports = function(){
     var express = require('express');
     var router = express.Router();
 
-    /* get people to populate in dropdown */
+    /* get assistants to populate in dropdown */
     function getAssistants(res, mysql, context, complete){
         mysql.pool.query("SELECT assist_id AS aid, assist_name FROM dc_dentAssist", function(error, results, fields){
             if(error){
                 res.write(JSON.stringify(error));
                 res.end();
             }
-            context.people = results;
+            context.assistants = results;
             complete();
         });
     }
 
-    /* get certificates to populate in dropdown */
+    /* get appointments to populate in dropdown */
     function getAppointments(res, mysql, context, complete){
         sql = "SELECT appt_id AS apid, assist FROM dc_appt";
         mysql.pool.query(sql, function(error, results, fields){
@@ -27,10 +27,8 @@ module.exports = function(){
         });
     }
 
-    /* get people with their certificates */
-    /* TODO: get multiple certificates in a single column and group on
-     * assist_name or id column
-     */
+    /* get assistants with their appointments */
+    /* get multiple appointments in a single column and group on assist_name or id column */
     function getAssistantsWithAppointments(res, mysql, context, complete){
         sql = "SELECT aid, apid, CONCAT(assist_name) AS name, assist AS assistant FROM dc_dentAssist INNER JOIN dc_appt_assist on dc_dentAssist.assist_id = dc_appt_assist.aid INNER JOIN dc_appt on dc_appt.appt_id = dc_appt_assist.apid ORDER BY name, assistant"
          mysql.pool.query(sql, function(error, results, fields){
@@ -44,9 +42,7 @@ module.exports = function(){
     }
 
 
-    /* List people with certificates along with
-     * displaying a form to associate a person with multiple certificates
-     */
+    /* List assistants with appointments along with displaying a form to associate a person with multiple appointments */
     router.get('/', function(req, res){
         var callbackCount = 0;
         var context = {};
@@ -65,13 +61,11 @@ module.exports = function(){
         }
     });
 
-    /* Associate certificate or certificates with a person and
-     * then redirect to the assistant_with_appts page after adding
-     */
+    /* Associate appointment or appointments with an assistant  and then redirect to the assistant_with_appts page after adding */
     router.post('/', function(req, res){
         console.log("We get the multi-select assistant dropdown as ", req.body.appts)
         var mysql = req.app.get('mysql');
-        // let's get out the certificates from the array that was submitted by the form
+        // let's get out the appointments from the array that was submitted by the form
         var appointments = req.body.appts
         var person = req.body.aid
         for (let appt of appointments) {
@@ -92,7 +86,7 @@ module.exports = function(){
         res.redirect('/appt_assist');
     });
 
-    /* Delete a person's certification record */
+    /* Delete an assistant's appointment record */
     /* This route will accept a HTTP DELETE request in the form
      * /pid/{{pid}}/appt/{{apid}} -- which is sent by the AJAX form
      */
